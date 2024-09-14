488b39a4bb93500ad45ad990146afe51fe3048d2
Hejiahao <14858979+hzzz-lx@user.noreply.gitee.com>
2024-09-14 17:16:22 +0800

    底部

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <!-- <HomePage></HomePage> -->
+  <HomePage></HomePage>
+  <Bottom></Bottom>
   <!-- <div ref="elref" class="w-[100vw] overflow-hidden">
     <div class="flex w-[240vw] h-[50vw]">
       <div class="w-[80vw] bg-green-200">1</div>
@@ -7,14 +8,15 @@
       <div class="w-[80vw] bg-green-400">3</div>
     </div>
   </div> -->
-  <BetterScoll class="h-[20vw] bg-green-400">
+  <!-- <BetterScoll class="h-[20vw] bg-green-400">
     <div class="h-[80vw] bg-green-200">1</div>
     <div class="h-[80vw] bg-green-300">2</div>
     <div class="h-[80vw] bg-green-400">3</div>
-  </BetterScoll>
+  </BetterScoll> -->
 </template>
 <script setup>
-// import HomePage from './views/HomePage.vue'
+import HomePage from './views/HomePage.vue'
+import Bottom from './views/Bottom.vue'
 // import { ref, onMounted } from 'vue'
 import BetterScoll from './components/BetterScoll.vue'
 </script>
--- src/views/Bottom.vue
+++ src/views/Bottom.vue
@@ -1,3 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <div class="flex text-white">
+      <div class="flex flex-col items-center w-[20vw]">
+        <Icon icon="simple-icons:neteasecloudmusic"  style="color: black" class="w-[9vw] h-[9vw] filter" />
+        <span class="m-3 text-[4vw] text-[#676c6c]">发现</span>
+      </div>
+      <div class="flex flex-col items-center w-[20vw]">
+        <Icon icon="basil:hotspot-solid" style="color: black" class="w-[9vw] h-[9vw] filter" />
+        <span class="m-3 text-[4vw] text-[#676c6c]">博客</span>
+      </div>
+      <div class="flex flex-col items-center w-[20vw]">
+        <Icon icon="mingcute:music-line"  style="color: black"  class="w-[9vw] h-[9vw] filter" />
+        <span class="m-3 text-[4vw] text-[#676c6c]">我的</span>
+      </div>
+      <div class="flex flex-col items-center w-[20vw]">
+        <Icon icon="ic:baseline-group" style="color: black" class="w-[9vw] h-[9vw] filter" />
+        <span class="m-3 text-[4vw] text-[#676c6c]">关注</span>
+      </div>
+      <div class="flex flex-col items-center w-[20vw]">
+        <Icon icon="basil:comment-solid" style="color: black" class="w-[9vw] h-[9vw] filter" />
+        <span class="m-3 text-[4vw] text-[#676c6c]">社区</span>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+import { Icon } from '@iconify/vue'
+</script>
+<style scoped>
+.filter {
+  filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='colorize'><feColorMatrix type='matrix' values='1 0 0 0 0.698 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0' /></filter></svg>#colorize");
+}
+</style>
