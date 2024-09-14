ffab5eeba253bfbab8f31ea588496cf0fe818508
xl <173978372@qq.com>
2024-09-14 11:32:16 +0800

    c1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <HomePage></HomePage>
+  <!-- <HomePage></HomePage> -->
   <!-- <div ref="elref" class="w-[100vw] overflow-hidden">
     <div class="flex w-[240vw] h-[50vw]">
       <div class="w-[80vw] bg-green-200">1</div>
@@ -7,8 +7,14 @@
       <div class="w-[80vw] bg-green-400">3</div>
     </div>
   </div> -->
+  <BetterScoll class="h-[20vw] bg-green-400">
+    <div class="h-[80vw] bg-green-200">1</div>
+    <div class="h-[80vw] bg-green-300">2</div>
+    <div class="h-[80vw] bg-green-400">3</div>
+  </BetterScoll>
 </template>
 <script setup>
-import HomePage from './views/HomePage.vue'
+// import HomePage from './views/HomePage.vue'
 // import { ref, onMounted } from 'vue'
+import BetterScoll from './components/BetterScoll.vue'
 </script>
---
+++ src/components/BetterScoll.vue
@@ -0,0 +1,33 @@
+<template>
+  <div ref="werper" class="overflow-hidden">
+    <div class="">
+      <slot />
+    </div>
+  </div>
+</template>
+<script setup>
+import BScroll from '@better-scroll/core'
+import { ref, onMounted, defineProps, onBeforeUnmount } from 'vue'
+const werper = ref(null)
+let bs = null
+const props = defineProps({
+  type: {
+    type: Object,
+    default: () => {
+      return {
+        scrollX: false,
+        scrollY: true
+      }
+    }
+  }
+})
+const app = () => {
+  if (screenX) {
+    return
+  }
+}
+onMounted(() => {
+  bs = new BScroll(werper.value, props.type)
+})
+onBeforeUnmount(() => bs.destroy())
+</script>
